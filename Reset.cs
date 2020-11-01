f1121d1919cef0979ca4221104024332dea2c292
Solo1234321 <73799969+Solo1234321@users.noreply.github.com>
2020-11-01 19:53:01 -0300

    Add files via upload

---
+++ Reset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reset : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(
+    }
+}

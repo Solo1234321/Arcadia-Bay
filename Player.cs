3c8f7a0c4cc3e71e761d066700d87447371aa47b
Solo1234321 <73799969+Solo1234321@users.noreply.github.com>
2020-11-01 19:51:46 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private const float Z = 0f;
+    public float speed;
+    private Rigidbody2D rig;
+    public float JumpForce;
+    private bool IsJumping;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+      
+
+        if (Input.GetMouseButtonDown (0) && !IsJumping)
+        {
+            rig.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            IsJumping = true;
+        }
+
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisor)
+    {
+      if(colisor.gameObject.layer == 8)
+        {
+            IsJumping = false;
+        }
+    }
+    void Move()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0f, z: 0f);
+               transform.position += Movement * Time.deltaTime * speed;
+    }
+}
